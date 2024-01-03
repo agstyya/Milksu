08524661d954bcde307c04461aa6a900eaed8477
agstyya <hendraagung621@gmail.com>
2024-01-03 23:40:58 +0700

    menambahkan keranjang belanja

--- js/script.js
+++ js/script.js
@@ -1,16 +1,71 @@
-// Toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
 // ketika Hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = (e) => {
+  navbarNav.classList.toggle('active');
+  e.preventDefault();
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
+// toggle kelas active untuk search form
 
-const hamburger = document.querySelector("#hamburger-menu");
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.targer)) {
-    navbarNav.classList.remove("active");
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle kelas active
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Klik diluar elemen
+
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
   }
 });
+
+// modal box
+
+const itemDetailModal = document.querySelector('#item-detail-modal');
+
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+
+// klik tomblo close modal 
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik dilua modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal){
+    itemDetailModal.style.display = 'none';
+  }
+};
---
+++ src/app.js
@@ -0,0 +1,124 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('products', () => ({
+        items: [
+            { id: 1, name: 'Milksu', img: '1.jpg', price: 40000 },
+            { id: 2, name: 'Greenfields', img: '2.jpg', price: 20000 },
+            { id: 3, name: 'CowCow', img: '3.jpg', price: 35000 },
+            { id: 4, name: 'Milkboom', img: '4.jpg', price: 30000 },
+            { id: 5, name: 'Milkcow', img: '5.jpg', price: 25000 },
+            { id: 5, name: 'Mahasusu', img: '5.jpg', price: 60000 },
+        ],
+    }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+
+            // cek apakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+            // jika belum ada /cart kosong
+            if(!cartItem){
+                this.items.push({...newItem, quantity: 1, total: newItem.price });
+                this.quantity++;
+                this.total += newItem.price;
+                
+            } else{
+                // jika barang sudah ada
+                this.items = this.items.map((item) => {
+                    // jika barang beda
+                    if(item.id !==newItem.id){
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah jumlah dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+        remove(id){
+            // ambil item yang mau diremove berdasarkan id nya 
+            const cartItem = this.items.find((items) => items.id === id);
+            // jika item lebih dari satu 
+            if(cartItem.quantity > 1){
+                // Telusuri 
+                this.items = this.items.map((item) => {
+                    // jikabukan barang yang di klik 
+                    if(item.id !== id){
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1){
+                // jika barang sisa satu
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
+        }
+    });
+});
+
+// Form Validasi
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function(){
+    for(let i = 0; i < form.elements.length; i++){
+        if(form.elements[i].value.length !== 0){
+            checkoutButton.classList.remove('disabled');
+            checkoutButton.classList.add('disabled');
+        } else{
+            return false;
+        }
+    }
+    checkoutButton.disabled = false;
+    checkoutButton.classList.remove('disabled');
+}); 
+
+// kirim data ketika tombol di klik
+
+checkoutButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    const objData = Object.fromEntries(data);
+    const message = formatMessage(objData);
+    window.open('http://wa.me/6283179757966?text=' + encodeURIComponent(message));
+});
+
+// format pesan whatsapp
+const formatMessage = (obj) => {
+    return `Data Customer =
+
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+
+Data Pesanan =
+
+    ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
+    TOTAL: ${rupiah(obj.total)}
+    Terima Kasih.`;
+};
+
+// konversi ke rupiah 
+const rupiah =(number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(number);
+};
